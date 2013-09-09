b92db39fcd8caa47cacfdeca1bedef50e35e914e
Cyril Pichard <cyril.pichard@primefocusworld.com>
2013-09-09 19:15:53 +0100

    refactoring: moved colorspace related functions in a new file ColorSpace.cpp

--- src/duke/engine/ColorSpace.hpp
+++ src/duke/engine/ColorSpace.hpp
@@ -7,4 +7,13 @@ enum class ColorSpace
 		Auto, Linear, sRGB, GammaCorrected, AdobeRGB, Rec709, KodakLog, AlexaLogC, _END
 };
 
+// Deduce a the colorspace
+ColorSpace resolveFromExtension(const char* pFileExtension);
+ColorSpace resolveFromName(const char* pColorspace);
+
+
+// GlSl functions names
+const char* getToLinearFunction(const ColorSpace fromColorspace);
+const char* getToScreenFunction(const ColorSpace fromColorspace);
+
 } /* namespace duke */
