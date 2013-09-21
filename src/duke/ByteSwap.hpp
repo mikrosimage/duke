deb9526d1b78b7ef023e687a045604a04d27f932
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2013-09-21 15:58:00 +0200

    Fix warning - removing unused function

--- src/duke/ByteSwap.hpp
+++ src/duke/ByteSwap.hpp
@@ -10,8 +10,4 @@ inline unsigned int bswap_32(unsigned int x) {
     return (bswap_16(x & 0xffff) << 16) | (bswap_16(x >> 16));
 }
 
-inline unsigned long long bswap_64(unsigned long long x) {
-    return (((unsigned long long) bswap_32(x & 0xffffffffull)) << 32) | (bswap_32(x >> 32));
-}
-
 }  // namespace
