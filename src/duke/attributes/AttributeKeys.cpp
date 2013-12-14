5c693e3c9396d6a8b966d3674bfc50b71b83c5f2
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2013-12-14 17:44:13 +0100

    removing unnecessary keys

--- src/duke/attributes/AttributeKeys.cpp
+++ src/duke/attributes/AttributeKeys.cpp
@@ -3,9 +3,7 @@
 namespace attribute {
 
 const char pDukeError[] = "duke:error";
-const char pDukeFileExtensionKey[] = "duke:FileExtension";
 const char pDukeFilePathKey[] = "duke:FilePath";
-const char pDukeFileNameKey[] = "duke:FileName";
 const char pOiioColospaceKey[] = "oiio:ColorSpace";
 const char pOiioGammaKey[] = "oiio:Gamma";
 const char pOrientationKey[] = "Orientation";
--- src/duke/attributes/AttributeKeys.hpp
+++ src/duke/attributes/AttributeKeys.hpp
@@ -3,9 +3,7 @@
 namespace attribute {
 
 extern const char pDukeError[];
-extern const char pDukeFileExtensionKey[];
 extern const char pDukeFilePathKey[];
-extern const char pDukeFileNameKey[];
 extern const char pOiioColospaceKey[];
 extern const char pOiioGammaKey[];
 extern const char pOrientationKey[];
