5e6a5e9abf34626ae65aadcdb0fb0a0a9a5d9587
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2014-01-19 17:13:28 +0100

    Simplifying design

--- src/duke/attributes/Attributes.hpp
+++ src/duke/attributes/Attributes.hpp
@@ -7,51 +7,31 @@
 #include <vector>
 #include <algorithm>
 
-namespace attribute {
-
-namespace details {
-
-typedef SmallVector<char> AttributeData;
-
-template<class T>
-inline AttributeData dematerialize(const T& value) {
-    static_assert(std::is_pod<T>::value, "Type has to be Plain Old Data");
-    return {reinterpret_cast<const char*>(&value), sizeof(value)};
-}
-
-template<>
-inline AttributeData dematerialize<const char*>(const char* const & value) {
-    return {value, strlen(value) + 1};
-}
-
-template<>
-inline AttributeData dematerialize<std::string>(const std::string& value) {
-    return {value.data(), value.size()};
-}
-
-template<typename T>
-inline T materialize(const void* ptr, size_t size) {
-    CHECK(sizeof(T) == size);
-    return *reinterpret_cast<const T*>(ptr);
-}
-
-template<>
-inline const char* materialize(const void* ptr, size_t size) {
-    return reinterpret_cast<const char*>(ptr);
-}
-
-template<>
-inline std::string materialize<std::string>(const void* ptr, size_t size) {
-    return std::string(reinterpret_cast<const char*>(ptr), size);
-}
-
-}  // namespace details
-
-}  // namespace attribute
-
 struct Attributes {
 private:
-    typedef std::tuple<const char*, const void*, attribute::details::AttributeData> MapEntry;
+    typedef SmallVector<char> AttributeData;
+    template<class T>
+    static inline AttributeData dematerialize(const T& value) {
+        static_assert(std::is_pod<T>::value, "Type has to be Plain Old Data");
+        return {reinterpret_cast<const char*>(&value), sizeof(value)};
+    }
+
+    template<typename T>
+    static inline T materialize(const void* ptr, size_t size) {
+        CHECK(sizeof(T) == size);
+        return *reinterpret_cast<const T*>(ptr);
+    }
+
+    struct MapEntry {
+        const char* pKey;
+        const void* pDescriptor;
+        AttributeData attributeData;
+        MapEntry() : MapEntry(nullptr, nullptr, {}) {}
+        MapEntry(const char* pKey, const void* pDescriptor, AttributeData attributeData) :
+                        pKey(pKey), pDescriptor(pDescriptor), attributeData(attributeData) {
+        }
+    };
+
     typedef std::vector<MapEntry> MapImpl;
     typedef MapImpl::const_iterator const_iterator;
     typedef MapImpl::iterator iterator;
@@ -59,26 +39,18 @@ private:
 
     MapImpl m_Map;
 
-    inline static const char*& key(value_type& value) {return std::get<0>(value);}
-    inline static const void*& descriptor(value_type& value) {return std::get<1>(value);}
-    inline static attribute::details::AttributeData& data(value_type& value) {return std::get<2>(value);}
-
-    inline static const char* key(const value_type& value) {return std::get<0>(value);}
-    inline static const void* descriptor(const value_type& value) {return std::get<1>(value);}
-    inline static const attribute::details::AttributeData& data(const value_type& value) {return std::get<2>(value);}
-
     inline const_iterator find(const char* pKey) const {
         return std::find_if(m_Map.begin(), m_Map.end(),
-                            [=](const value_type& a) {return streq(key(a), pKey);});
+                            [=](const value_type& a) {return streq(a.pKey, pKey);});
     }
 
     inline iterator find(const char* pKey) {
         return std::find_if(m_Map.begin(), m_Map.end(),
-                            [=](const value_type& a) {return streq(key(a), pKey);});
+                            [=](const value_type& a) {return streq(a.pKey, pKey);});
     }
 
     inline static bool isValid(const MapEntry& entry) {
-        return key(entry);// && descriptor(entry);
+        return entry.pKey;// && descriptor(entry);
     }
 
 public:
@@ -109,20 +81,18 @@ public:
 
     template<typename KEY>
     typename KEY::value_type getOrDie() const {
-        using namespace attribute::details;
         const MapEntry& entry(get(KEY().name()));
-        CHECK(descriptor(entry) == KEY().descriptor());
-        const AttributeData& attributeData = data(entry);
+        CHECK(entry.pDescriptor == KEY().descriptor());
+        const AttributeData& attributeData = entry.attributeData;
         return materialize<typename KEY::value_type>(attributeData.data(), attributeData.size());
     }
 
     template<typename KEY>
     typename KEY::value_type getWithDefault(const typename KEY::value_type& default_value) const {
-        using namespace attribute::details;
         const MapEntry& entry(get(KEY().name()));
         if (!isValid(entry)) return default_value;
-        CHECK(descriptor(entry) == KEY().descriptor());
-        const AttributeData& attributeData = data(entry);
+        CHECK(entry.pDescriptor == KEY().descriptor());
+        const AttributeData& attributeData = entry.attributeData;
         return materialize<typename KEY::value_type>(attributeData.data(), attributeData.size());
     }
 
@@ -132,26 +102,24 @@ public:
     }
 
     void set(const char* pKey, const void *pDescriptor, const char* pData, const size_t size) {
-        using namespace attribute::details;
         auto pFound = find(pKey);
         if (pFound == end())
             m_Map.emplace_back(pKey, pDescriptor, AttributeData { pData, size });
         else {
-            descriptor(*pFound) = pDescriptor;
-            data(*pFound) = AttributeData { pData, size };
+            pFound->pDescriptor = pDescriptor;
+            pFound->attributeData = AttributeData { pData, size };
         }
     }
 
     template<typename KEY>
     void set(const typename KEY::value_type& value) {
-        using namespace attribute::details;
         const char* pKey = KEY().name();
         const auto pFound = find(pKey);
         if (pFound == end())
             m_Map.emplace_back(pKey, KEY().descriptor(), dematerialize<typename KEY::value_type>(value));
         else {
-            descriptor(*pFound) = KEY().descriptor();
-            data(*pFound) = dematerialize<typename KEY::value_type>(value);
+            pFound->pDescriptor = KEY().descriptor();
+            pFound->attributeData = dematerialize<typename KEY::value_type>(value);
         }
     }
 
@@ -174,3 +142,21 @@ public:
         return m_Map.size();
     }
 };
+
+template<>
+inline Attributes::AttributeData Attributes::dematerialize<const char*>(const char* const & value) {
+    return {value, strlen(value) + 1};
+}
+template<>
+inline Attributes::AttributeData Attributes::dematerialize<std::string>(const std::string& value) {
+    return {value.data(), value.size()};
+}
+
+template<>
+inline const char* Attributes::materialize(const void* ptr, size_t size) {
+    return reinterpret_cast<const char*>(ptr);
+}
+template<>
+inline std::string Attributes::materialize<std::string>(const void* ptr, size_t size) {
+    return std::string(reinterpret_cast<const char*>(ptr), size);
+}
