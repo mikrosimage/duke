98ce22428e76704005a845ab39fc1f2bff5587d3
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2014-03-08 15:35:47 +0100

    Improve CHECK function

--- src/duke/base/Check.hpp
+++ src/duke/base/Check.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include <sstream>
 
 #ifdef __GNUC__
 #define DUKE_LIKELY(x)   (__builtin_expect((x), 1))
@@ -16,4 +17,21 @@
                              __func__, \
                              __FILE__, \
                              __LINE__)
-#define CHECK(X) if(DUKE_UNLIKELY(!(X))) { TRACE_MSG(X); abort(); }
+
+struct Tracer {
+	Tracer(const char* cond, const char* func, const char* file,
+			const int line) {
+		msg << "CHECK FAILED '" << cond << "' in " << func << "() [" << file << ":" << line << "] ";
+	}
+	std::ostream& getStream() {
+		return msg;
+	}
+	~Tracer() {
+		fprintf(stderr, "%s\n", msg.str().c_str());
+		abort();
+	}
+private:
+	std::ostringstream msg;
+};
+
+#define CHECK(X) if(DUKE_UNLIKELY(!(X))) Tracer(#X,__func__, __FILE__, __LINE__).getStream()
