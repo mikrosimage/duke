e23d50d4f2b5452277da2196f56ee9b715abc4d3
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2014-04-30 23:21:50 +0200

    Formatting parameters

--- src/duke/engine/parameters/Parameters.cpp
+++ src/duke/engine/parameters/Parameters.cpp
@@ -1,12 +1,12 @@
 #include "Parameters.hpp"
 
 Parameters::Parameters() {
-	add( { "gamma", float_value, "1" });
-	add( { "exposure", float_value, "1" });
-	add( { "channel", string_value });
-	add( { "colorspace", string_value });
-	add( { "lut", string_value });
-	add( { "framerate", string_value });
-	add( { "zoom", float_value, "1" });
-	add( { "currentframe", int_value });
+  add({"gamma", float_value, "1"});
+  add({"exposure", float_value, "1"});
+  add({"channel", string_value});
+  add({"colorspace", string_value});
+  add({"lut", string_value});
+  add({"framerate", string_value});
+  add({"zoom", float_value, "1"});
+  add({"currentframe", int_value});
 }
--- src/duke/engine/parameters/Parameters.hpp
+++ src/duke/engine/parameters/Parameters.hpp
@@ -2,17 +2,18 @@
 #include <duke/commands/DescriptorSet.hpp>
 #include <string>
 
-enum ParameterType
-	:char {
-		int_value, float_value, string_value
+enum ParameterType : char {
+  int_value,
+  float_value,
+  string_value
 };
 
 struct ParameterDescription {
-	std::string name;
-	ParameterType type;
-	std::string defaultValue;
+  std::string name;
+  ParameterType type;
+  std::string defaultValue;
 };
 
-struct Parameters: public DescriptorSet<ParameterDescription> {
-	Parameters();
+struct Parameters : public DescriptorSet<ParameterDescription> {
+  Parameters();
 };
