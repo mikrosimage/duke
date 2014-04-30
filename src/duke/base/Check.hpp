5efe1e88b2d205b3de9b13851336b2451a55dbf6
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2014-04-30 23:00:02 +0200

    Formatting base

--- src/duke/base/Check.hpp
+++ src/duke/base/Check.hpp
@@ -12,7 +12,11 @@
 #define DUKE_UNLIKELY(x) (x)
 #endif
 
-#define LOG_FATAL(COND,FUNC,FILE,LINE) { fprintf(stderr, "CHECK FAILED '%s' in %s() [%s:%d]\n", COND, FUNC, FILE, LINE); abort(); }
+#define LOG_FATAL(COND, FUNC, FILE, LINE)                                           \
+  {                                                                                 \
+    fprintf(stderr, "CHECK FAILED '%s' in %s() [%s:%d]\n", COND, FUNC, FILE, LINE); \
+    abort();                                                                        \
+  }
 #define TRACE_MSG(X) fprintf(stderr, "CHECK FAILED '" #X "' in %s() [%s:%d]\n", __func__, __FILE__, __LINE__)
 
 struct Tracer {
@@ -33,8 +37,10 @@ struct Tracer {
 #define CHECK(X) \
   if (DUKE_UNLIKELY(!(X))) Tracer(#X, __func__, __FILE__, __LINE__).getStream()
 
-template<typename T>
-inline T& _checkNotNull(const char* cond, const char* func, const char* file, const int line, T& ptr){ if (DUKE_UNLIKELY(!ptr)) LOG_FATAL(cond, func, file, line); return ptr; }
+template <typename T>
+inline T& _checkNotNull(const char* cond, const char* func, const char* file, const int line, T& ptr) {
+  if (DUKE_UNLIKELY(!ptr)) LOG_FATAL(cond, func, file, line);
+  return ptr;
+}
 
-#define CHECK_NOTNULL(X) \
-    _checkNotNull(#X " must not be nullptr", __func__, __FILE__, __LINE__, X)
+#define CHECK_NOTNULL(X) _checkNotNull(#X " must not be nullptr", __func__, __FILE__, __LINE__, X)
