f87d742f69c208e276d9c522e3fc3d31ceb9bc65
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2014-04-30 23:01:00 +0200

    Formatting cmdline

--- src/duke/cmdline/CmdLineParameters.cpp
+++ src/duke/cmdline/CmdLineParameters.cpp
@@ -16,90 +16,87 @@ namespace duke {
 namespace {
 
 void checkArgCount(const int argc, int i) {
-    if (i >= argc) throw logic_error("missing command line value");
+  if (i >= argc) throw logic_error("missing command line value");
 }
 
-template<typename T>
-void getArgs(const int argc, const char* const * argv, int i, T& value) {
-    checkArgCount(argc, i);
-    istringstream iss(argv[i]);
-    iss >> value;
-    if (iss.fail()) throw logic_error("bad command line argument type");
+template <typename T>
+void getArgs(const int argc, const char* const* argv, int i, T& value) {
+  checkArgCount(argc, i);
+  istringstream iss(argv[i]);
+  iss >> value;
+  if (iss.fail()) throw logic_error("bad command line argument type");
 }
-template<>
-void getArgs<string>(const int argc, const char* const * argv, int i, string& value) {
-    checkArgCount(argc, i);
-    value = argv[i];
+
+template <>
+void getArgs<string>(const int argc, const char* const* argv, int i, string& value) {
+  checkArgCount(argc, i);
+  value = argv[i];
 }
 
 bool matches(const string option, const string longOption, const string shortOption = "") {
-    return option == shortOption || option == longOption;
+  return option == shortOption || option == longOption;
 }
 
 }  // namespace
 
-unsigned CmdLineParameters::getDefaultConcurrency() {
-    return max(1u, min(4u, thread::hardware_concurrency() - 2));
-}
+unsigned CmdLineParameters::getDefaultConcurrency() { return max(1u, min(4u, thread::hardware_concurrency() - 2)); }
 
 size_t CmdLineParameters::getDefaultCacheSize() {
-    return 500 * 1024 * 1024; // 500MiB
+  return 500 * 1024 * 1024;  // 500MiB
 }
 
-CmdLineParameters::CmdLineParameters(int argc, const char* const * argv) {
-    for (int i = 1; i < argc; ++i) {
-        const char* pOption = argv[i];
-        if (matches(pOption, "--swapinterval"))
-            getArgs(argc, argv, ++i, swapBufferInterval);
-        else if (matches(pOption, "--fullscreen", "-f"))
-            fullscreen = true;
-        else if (matches(pOption, "--unlimited"))
-            unlimitedFPS = true;
-        else if (matches(pOption, "--threads", "-t"))
-            getArgs(argc, argv, ++i, workerThreadDefault);
-        else if (matches(pOption, "--max-cache-size")) {
-            imageCacheSizeDefault = getTotalSystemMemory() * 80 / 100;
-        } else if (matches(pOption, "--cache-size", "-s")) {
-            getArgs(argc, argv, ++i, imageCacheSizeDefault);
-            imageCacheSizeDefault *= 1024 * 1024;
-        } else if (matches(pOption, "--framerate")) {
-            string arg;
-            getArgs(argc, argv, ++i, arg);
-            if (arg == "noskip") {
-                unlimitedFPS = true;
-            } else {
-                istringstream stream(arg);
-                stream >> defaultFrameRate;
-                if (stream.fail()) throw logic_error("invalid framerate");
-                defaultFrameRate.assign(defaultFrameRate.denominator(), defaultFrameRate.numerator());
-            }
-        } else if (matches(pOption, "--benchmark"))
-            mode = ApplicationMode::BENCHMARK;
-        else if (matches(pOption, "--help", "-h"))
-            mode = ApplicationMode::HELP;
-        else if (matches(pOption, "--version", "-v"))
-            mode = ApplicationMode::VERSION;
-        else if (matches(pOption, "--list", "-l"))
-            mode = ApplicationMode::LIST_SUPPORTED_FORMAT;
-        else if (matches(pOption, "--inputspace")) {
-			std::string colorSpaceString;
-			getArgs(argc, argv, ++i, colorSpaceString);
-			inputColorSpace = resolveFromName(colorSpaceString.c_str());
-		}
-        else if (matches(pOption, "--outputspace")) {
-			std::string colorSpaceString;
-			getArgs(argc, argv, ++i, colorSpaceString);
-			outputColorSpace = resolveFromName(colorSpaceString.c_str());
-		}
-        else if (*pOption != '-')
-            additionnalOptions.push_back(pOption);
-        else
-            throw logic_error(string("unknown command line argument '") + pOption + "'");
-    }
+CmdLineParameters::CmdLineParameters(int argc, const char* const* argv) {
+  for (int i = 1; i < argc; ++i) {
+    const char* pOption = argv[i];
+    if (matches(pOption, "--swapinterval"))
+      getArgs(argc, argv, ++i, swapBufferInterval);
+    else if (matches(pOption, "--fullscreen", "-f"))
+      fullscreen = true;
+    else if (matches(pOption, "--unlimited"))
+      unlimitedFPS = true;
+    else if (matches(pOption, "--threads", "-t"))
+      getArgs(argc, argv, ++i, workerThreadDefault);
+    else if (matches(pOption, "--max-cache-size")) {
+      imageCacheSizeDefault = getTotalSystemMemory() * 80 / 100;
+    } else if (matches(pOption, "--cache-size", "-s")) {
+      getArgs(argc, argv, ++i, imageCacheSizeDefault);
+      imageCacheSizeDefault *= 1024 * 1024;
+    } else if (matches(pOption, "--framerate")) {
+      string arg;
+      getArgs(argc, argv, ++i, arg);
+      if (arg == "noskip") {
+        unlimitedFPS = true;
+      } else {
+        istringstream stream(arg);
+        stream >> defaultFrameRate;
+        if (stream.fail()) throw logic_error("invalid framerate");
+        defaultFrameRate.assign(defaultFrameRate.denominator(), defaultFrameRate.numerator());
+      }
+    } else if (matches(pOption, "--benchmark"))
+      mode = ApplicationMode::BENCHMARK;
+    else if (matches(pOption, "--help", "-h"))
+      mode = ApplicationMode::HELP;
+    else if (matches(pOption, "--version", "-v"))
+      mode = ApplicationMode::VERSION;
+    else if (matches(pOption, "--list", "-l"))
+      mode = ApplicationMode::LIST_SUPPORTED_FORMAT;
+    else if (matches(pOption, "--inputspace")) {
+      std::string colorSpaceString;
+      getArgs(argc, argv, ++i, colorSpaceString);
+      inputColorSpace = resolveFromName(colorSpaceString.c_str());
+    } else if (matches(pOption, "--outputspace")) {
+      std::string colorSpaceString;
+      getArgs(argc, argv, ++i, colorSpaceString);
+      outputColorSpace = resolveFromName(colorSpaceString.c_str());
+    } else if (*pOption != '-')
+      additionnalOptions.push_back(pOption);
+    else
+      throw logic_error(string("unknown command line argument '") + pOption + "'");
+  }
 }
 
 void CmdLineParameters::printHelpMessage() const {
-    printf(R"(Usage: duke [OPTION]... [FILE/FOLDER]...
+  printf(R"(Usage: duke [OPTION]... [FILE/FOLDER]...
   -h, --help                 display this help and exit
   -v, --version              output version information and exit
       --benchmark            tests current machine's performance.
@@ -127,7 +124,7 @@ void CmdLineParameters::printHelpMessage() const {
   -t, --threads SIZE         specify the number of decoding threads,
                              defaults to %u for this machine.
 )",
-           getDefaultCacheSize() / (1024 * 1024), getDefaultConcurrency());
+         getDefaultCacheSize() / (1024 * 1024), getDefaultConcurrency());
 }
 
 }  // namespace duke
--- src/duke/cmdline/CmdLineParameters.hpp
+++ src/duke/cmdline/CmdLineParameters.hpp
@@ -9,32 +9,34 @@
 
 namespace duke {
 
-struct commandline_error: std::runtime_error {
-	commandline_error(const std::string& msg) :
-			std::runtime_error(msg) {
-	}
+struct commandline_error : std::runtime_error {
+  commandline_error(const std::string& msg) : std::runtime_error(msg) {}
 };
 
 enum class ApplicationMode {
-	DUKE, BENCHMARK, HELP, VERSION, LIST_SUPPORTED_FORMAT
+  DUKE,
+  BENCHMARK,
+  HELP,
+  VERSION,
+  LIST_SUPPORTED_FORMAT
 };
 
 struct CmdLineParameters {
-	CmdLineParameters(int argc, const char* const * argv);
-	void printHelpMessage() const;
-	unsigned swapBufferInterval = 1;
-	bool fullscreen = false;
-	bool unlimitedFPS = false;
-	unsigned workerThreadDefault = getDefaultConcurrency();
-	size_t imageCacheSizeDefault = getDefaultCacheSize();
-	ApplicationMode mode = ApplicationMode::DUKE;
-	FrameDuration defaultFrameRate = FrameDuration::PAL;
-	std::vector<std::string> additionnalOptions;
-	ColorSpace inputColorSpace = ColorSpace::Auto;
-	ColorSpace outputColorSpace = ColorSpace::Auto;
+  CmdLineParameters(int argc, const char* const* argv);
+  void printHelpMessage() const;
+  unsigned swapBufferInterval = 1;
+  bool fullscreen = false;
+  bool unlimitedFPS = false;
+  unsigned workerThreadDefault = getDefaultConcurrency();
+  size_t imageCacheSizeDefault = getDefaultCacheSize();
+  ApplicationMode mode = ApplicationMode::DUKE;
+  FrameDuration defaultFrameRate = FrameDuration::PAL;
+  std::vector<std::string> additionnalOptions;
+  ColorSpace inputColorSpace = ColorSpace::Auto;
+  ColorSpace outputColorSpace = ColorSpace::Auto;
 
-	static unsigned getDefaultConcurrency();
-	static size_t getDefaultCacheSize();
+  static unsigned getDefaultConcurrency();
+  static size_t getDefaultCacheSize();
 };
 
 }  // namespace duke
